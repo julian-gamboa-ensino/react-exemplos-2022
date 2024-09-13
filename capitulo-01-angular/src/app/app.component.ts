1a63e384f0f233ccca79e78b1f5dc94bfc4199d0
julian-gamboa-ensino <julian.gamboa.ms.2021@gmail.com>
2024-09-12 22:25:38 -0300

    component Login com aws cognito

--- capitulo-01-angular/src/app/app.component.ts
+++ capitulo-01-angular/src/app/app.component.ts
@@ -2,24 +2,24 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { BasicInfoComponent } from "./components/basic-info/basic-info.component";
-
 import { AllCertificatesComponent } from './components/certificates-section/all-certificates/all-certificates.component';
 import { BootcampCertificatesComponent } from './components/certificates-section/bootcamp-certificates/bootcamp-certificates.component';
+import { CertificatesSectionComponent } from './components/certificates-section/certificates-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BasicInfoComponent,
+  imports: [
+    RouterOutlet, 
     BasicInfoComponent,
-
     AllCertificatesComponent,
-    BootcampCertificatesComponent
-
-
+    BootcampCertificatesComponent,
+    CertificatesSectionComponent,
+    
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'capitulo-01-angular';
+  title = 'Setembro 12';
 }
--- capitulo-01-angular/src/app/components/certificates-section/certificates-section.component.ts
+++ capitulo-01-angular/src/app/components/certificates-section/certificates-section.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { BootcampCertificatesComponent } from "./bootcamp-certificates/bootcamp-certificates.component";
+import { AllCertificatesComponent } from "./all-certificates/all-certificates.component";
+
 
 @Component({
   selector: 'app-certificates-section',
   standalone: true,
-  imports: [],
+  imports: [BootcampCertificatesComponent, AllCertificatesComponent],
   templateUrl: './certificates-section.component.html',
   styleUrl: './certificates-section.component.scss'
 })
