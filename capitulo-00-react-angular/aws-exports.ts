22b652c06b31614f7ebb8aefb2b6392304a1eda7
julian-gamboa-ensino <julian.gamboa.ms.2021@gmail.com>
2024-09-16 20:41:34 -0300

    redirecionar e usar o  jwt https://legacy.reactjs.org/docs/higher-order-components.html

--- capitulo-00-react-angular/aws-exports.ts
+++ capitulo-00-react-angular/aws-exports.ts
@@ -1,9 +1,9 @@
 const awsmobile = {
-    "aws_project_region": "us-east-1", // Substitua pela sua região
-    "aws_cognito_identity_pool_id": "us-east-1:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", // Substitua pelo ID do seu Identity Pool (opcional)
-    "aws_cognito_region": "us-east-1", // Substitua pela sua região
-    "aws_user_pools_id": "us-east-1_xxxxxxxxx", // Substitua pelo ID do seu User Pool
-    "aws_user_pools_web_client_id": "xxxxxxxxxxxxxxxxxxxxxxxxxx", // Substitua pelo ID do seu cliente web
+    "aws_project_region": "us-west-2", // 
+    //"aws_cognito_identity_pool_id": "us-east-1:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", // Substitua pelo ID do seu Identity Pool (opcional)
+    "aws_cognito_region": "us-west-2", // Substitua pela sua região
+    "aws_user_pools_id": "us-west-2_WPgUGyt78",                          
+    "aws_user_pools_web_client_id": "6pp7bqtrir970u7rrn67nkktch", // Substitua pelo ID do seu cliente web
   };
   
   export default awsmobile;
