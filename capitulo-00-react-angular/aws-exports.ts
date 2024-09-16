a224a18beca00e5b1cf3d745bdd79a19d35f7c2c
julian-gamboa-ensino <julian.gamboa.ms.2021@gmail.com>
2024-09-16 20:08:33 -0300

    ver a documentação do configure https://ui.docs.amplify.aws/

---
+++ capitulo-00-react-angular/aws-exports.ts
@@ -0,0 +1,10 @@
+const awsmobile = {
+    "aws_project_region": "us-east-1", // Substitua pela sua região
+    "aws_cognito_identity_pool_id": "us-east-1:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx", // Substitua pelo ID do seu Identity Pool (opcional)
+    "aws_cognito_region": "us-east-1", // Substitua pela sua região
+    "aws_user_pools_id": "us-east-1_xxxxxxxxx", // Substitua pelo ID do seu User Pool
+    "aws_user_pools_web_client_id": "xxxxxxxxxxxxxxxxxxxxxxxxxx", // Substitua pelo ID do seu cliente web
+  };
+  
+  export default awsmobile;
+  
--- capitulo-00-react-angular/src/components/CognitoLogin/CognitoLogin.tsx
+++ capitulo-00-react-angular/src/components/CognitoLogin/CognitoLogin.tsx
@@ -1,21 +1,27 @@
+import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css'
+import awsconfig from '../../../aws-exports';   
+
+Amplify.configure(awsconfig);
 
 const CognitoLogin = () => {
-  return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        user ? (
-          <div>
-            <h1>Olá, {user.username}</h1>
-            <button onClick={signOut}>Sair</button>
-          </div>
-        ) : (
-          <div>Faça login para continuar</div>
-        )
-      )}
-    </Authenticator>
-  );
+
+
+    return (
+        <Authenticator>
+            {({ signOut, user }) => (
+                user ? (
+                    <div>
+                        <h1>Olá, {user.username}</h1>
+                        <button onClick={signOut}>Sair</button>
+                    </div>
+                ) : (
+                    <div>Faça login para continuar</div>
+                )
+            )}
+        </Authenticator>
+    );
 };
 
 export default CognitoLogin;
